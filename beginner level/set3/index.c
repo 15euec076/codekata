887d793645477f7952dc5c20c9c19e9c4bd7894a
15euec076 <32997235+15euec076@users.noreply.github.com>
2018-01-18 22:40:27 +0530

    Update index.c

--- beginner level/set3/index.c
+++ beginner level/set3/index.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-	int i,arr[10];
-	for(i=0;i<strlen(arr);i++)
+	int i,arr[10],n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
 	{
-		scanf("%d",&arr[i]);
+            scanf("%d",&arr[i]);
 	}
-	for(i=0;i<strlen(arr);i++)
+	for(i=0;i<n;i++)
 	{
-	  printf("%d",arr[i]);
-	  printf("\t");
-	  printf("%d",i);
-	  
+            
+            printf("%d",arr[i]);
+            printf("\t");
+           	printf("%d",i);
+           	printf("\n");
+	
 	}
 	return 0;
 }

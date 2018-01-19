0c3d4e5aea0c5f35bd66708e69fa11b773dfc45d
15euec076 <32997235+15euec076@users.noreply.github.com>
2018-01-19 15:24:08 +0530

    Create numerals.c

---
+++ beginner level/set 4/numerals.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	int i,count=0;
+	char a[20];
+	scanf("%[^\n]s",&a);
+	for(i=0;a[i]!= '\0';i++)
+	{
+	if(a[i]>='0' && a[i]<='9')
+	{
+	 count++;
+	}
+	}
+	printf("%d",count);
+	return 0;
+}

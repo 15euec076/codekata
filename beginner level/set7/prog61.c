d22c2b1a52330db20f5ac4ab9d76042ddf725fb4
15euec076 <32997235+15euec076@users.noreply.github.com>
2018-03-08 16:37:38 +0530

    Create prog61.c

---
+++ beginner level/set7/prog61.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+                int k,i;
+                char a[100];
+                scanf("%s",&a);
+                scanf("%d",&k);
+                for(i=0;i<k;i++)
+                {
+                 printf("%c",a[i]);
+                }
+	
+	return 0;
+}

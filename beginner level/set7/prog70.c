1c52ab8a28863f59da1ac808d222f1d865b5b118
15euec076 <32997235+15euec076@users.noreply.github.com>
2018-03-08 19:34:53 +0530

    Create prog70.c

---
+++ beginner level/set7/prog70.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+	int n,i,a,b,c;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		a=pow(2,i);
+		if(a>n)
+		{
+			b=i;
+			break;
+		}
+	}
+	c=pow(2,b);
+	printf("%d",c);
+	
+	return 0;
+}

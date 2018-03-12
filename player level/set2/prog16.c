c43599bee60c8a6f626ba80bcf0866aecb695e3e
15euec076 <32997235+15euec076@users.noreply.github.com>
2018-03-12 11:58:58 +0530

    Create prog16.c

---
+++ player level/set2/prog16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[10],n,i,res=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		res=res^a[i];
+	}
+	printf("%d",res);
+}

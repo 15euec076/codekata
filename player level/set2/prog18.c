951d0b290f74ffb5248f79cbd0b0f433d1ee4099
15euec076 <32997235+15euec076@users.noreply.github.com>
2018-05-24 23:11:39 +0530

    Create prog18.c

---
+++ player level/set2/prog18.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	int count=0,i,j;
+	char s[]="kabali";
+	char temp,a[100];
+	int n=strlen(s);
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(s[i]>s[j])
+			{
+				temp=s[i];
+				s[i]=s[j];
+				s[j]=temp;
+			}
+		}
+	}
+	int k=0;
+	char temp1;
+	while(k<5)
+	{
+	 scanf("%s",a);
+	 int l=strlen(a);
+	 for(i=0;i<l;i++)
+	 {
+	 	for(j=i+1;j<l;j++)
+	 	{
+	 	 if(a[i]>a[j])
+	 	 {          
+	 	 	temp1=a[i];
+	 		a[i]=a[j];
+	 		a[j]=temp1;
+	 	 }
+	 	}
+	 }
+	 	if(strcmp(s,a)==0)
+	 	{
+	 		count=count+1;
+	 	}
+	 	k++;
+	 }
+	 
+	printf("%d",count);
+	
+	// your code goes here
+	return 0;
+}
